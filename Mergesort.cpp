6a5bdb202b5cb56f4969eb3c008a8b57863783a4
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-04-17 16:50:52 +0700

    menambahkan fungsi for

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -22,4 +22,9 @@ void input() {
     cout << "\nInputkan Isi Element Array";
     cout << "\n==========================" << endl;
 
+    for (int i = 0; i < n; i++ ) {
+        cout << "Array index ke - " << i << ": ";
+        cin >> arr[i];
+    }
 }
+
